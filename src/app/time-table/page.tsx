8d3c1bdec7bea2bc7659be7526bbb1106bff6bb6
kamlesh sahani <alex20150001969@gmail.com>
2024-10-10 22:38:21 +0530

    edit department :

--- src/app/time-table/page.tsx
+++ src/app/time-table/page.tsx
@@ -5,6 +5,7 @@ const TimeTablePage = () => {
     <div className="flex flex-col flex-1 h-full bg-back justify-center">
       <TeacherSidebar />
       <div>
+        
       </div>
       <div className="flex flex-col  justify-center">
         <h1 className="text-2xl font-bold text-center my-4">
